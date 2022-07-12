5f011553fbe9d2c9102743fe660a44ee73d4a461
kyawmyohlaing <kyawmyohlaingygn4@gmail.com>
2022-07-12 19:22:35 +0630

    header section

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '/utils/colors.dart';
+import '/widgets/big_text.dart';
+import '/widgets/small_text.dart';
 
 class MainFoodPage extends StatefulWidget {
   MainFoodPage({Key? key}) : super(key: key);
@@ -22,18 +25,25 @@ class _MainFoodPageState extends State<MainFoodPage> {
           children: [
             Column(
               children: [
-                Text("Country"),
-                Text("City"),
-               
+              BigText(text: "Burma", color: AppColors.mainColor),
+              Row(
+                children: [
+                  SmallText(text: "Yangon", color: Colors.black54),
+                  Icon(Icons.arrow_drop_down_rounded),
+                ]
+              )
               ],
             ),
-            Container(
-              width: 45,
-              height: 45,
-               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: Colors.blue,
-                ),
+            Center(
+              child: Container(
+                width: 45,
+                height: 45,
+                child: Icon(Icons.search, color: Colors.white),
+                 decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: AppColors.mainColor,
+                  ),
+              )
             )
           ],
         )
