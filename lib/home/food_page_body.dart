5bcc1d1295d9f0222944f79c21c2cebb954c06b1
kyawmyohlaing <kyawmyohlaingygn4@gmail.com>
2022-07-13 20:46:44 +0630

    foodpagebody

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '/utils/colors.dart';
+import '/widgets/big_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   FoodPageBody({Key? key}) : super(key: key);
@@ -8,11 +10,14 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.redAccent,
       height:320,
       child: PageView.builder(
+        controller: pageController,
         itemCount: 5,
         itemBuilder: (context, position){
           return _buildPageItem(position);
@@ -21,20 +26,54 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     );
   }
   Widget _buildPageItem(int index){
-    return Container(
-       width: 220,
+    return Stack(
+      children: [
+        Container
+    (
+       height: 220,
        margin: EdgeInsets.only(left:5, right:5),
       decoration: BoxDecoration(
       borderRadius: BorderRadius.circular(30),
       color: index.isEven?Color(0xff69c5df):Color(0xFF9294cc),
       image: DecorationImage(
-        image: 
-        //AssetImage(
-        //  "assets/image/burger.png"
-        //)
-        NetworkImage('https://images.unsplash.com/photo-1655312214809-553e851782ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60')
+        fit: BoxFit.cover,
+        image: AssetImage(
+          "assets/image/unsplash.jpg"
+        )
+        //NetworkImage('https://images.unsplash.com/photo-1655312214809-553e851782ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8eGpQUjRobGtCR0F8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60')
       )
       ),
+      ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Container
+      (
+       height: 140,
+       margin: EdgeInsets.only(left:30, right:30, bottom: 15),
+      decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.white,),
+        ),
+
+       child: Container(
+        padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+        child: Column(
+          children: [
+            BigText(text: "Chinese Side"),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Wrap(
+                  children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor, size: 15,))
+                ),
+              ],
+            )
+          ]
+        ),
+      ),
+      
+      )
+      ],
       );
   }
 }
