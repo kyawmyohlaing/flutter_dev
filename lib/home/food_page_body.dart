6aa3f5b9cce99ad2beee876d043a2a469a8a823f
kyawmyohlaing <kyawmyohlaingygn4@gmail.com>
2022-07-15 20:33:53 +0630

    sliding and scaling Pageviewbuilder

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -13,6 +13,26 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue=0.0;
+  double _scaleFactor=0.8;
+  double _height=220;
+
+  @override
+  void initState(){
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue: pageController.page!;
+        print("Current value is "+_currPageValue.toString());
+      });
+    });
+  }
+
+  @override
+  void dispose(){
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,7 +48,29 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     );
   }
   Widget _buildPageItem(int index){
-    return Stack(
+    Matrix4 matrix = new Matrix4.identity();
+    if(index==_currPageValue.floor()){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }else if(index ==_currPageValue.floor()+1){
+      var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }else if(index ==_currPageValue.floor()-1){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }else {
+      var currScale=0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 0);
+    }
+    return Transform(
+      transform: matrix,
+      child: Stack
+    (
       children: [
         Container
     (
@@ -46,14 +88,30 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       )
       ),
       ),
+      
       Align(
         alignment: Alignment.bottomCenter,
         child: Container(
           height: 120,
           margin: EdgeInsets.only(left:30, right:30, bottom: 30),
           decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
+          borderRadius: BorderRadius.circular(20),
           color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xFFe8e8e8),
+              blurRadius: 5.0,
+              offset: Offset(0, 5),
+            ),
+            BoxShadow(
+              color: color.white,
+              offset: offset(-5,0)
+            ),
+            BoxShadow(
+              color: color.white,
+              offset: offset(5,0)
+            ),
+          ]
           ),
         child: Container(
           padding: EdgeInsets.only(top: 10, left: 15, right: 15),
@@ -100,6 +158,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       
       )
       ],
-      );
+      )
+    ); 
   }
 }
